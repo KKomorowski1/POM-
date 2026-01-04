0a800583f30fc51e413c5153240cbead3cf04088
kacper <kakacper23@gmail.com>
2026-01-04 20:57:54 +0100

    updated config

--- config/config.py
+++ config/config.py
@@ -4,4 +4,4 @@ class Config:
     BASE_URL = "https://www.saucedemo.com/"
     TIMEOUT = 10  # Seconds
     BROWSER = "chrome"  # options: chrome, firefox, edge
-    HEADLESS = os.getenv("HEADLESS", "False"). lower() == "True"
+    HEADLESS = os.getenv("HEADLESS", "False").lower() == "True"
